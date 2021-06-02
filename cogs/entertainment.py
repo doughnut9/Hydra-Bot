64cfc8e73ba0f953e511ec4acc2c2569ee8e20f3
doughnut9 <deo.soham.2005@gmail.com>
2021-06-02 14:53:43 +0530

    Avatar final edit.

--- cogs/entertainment.py
+++ cogs/entertainment.py
@@ -14,10 +14,14 @@ class Entertainment(commands.Cog):
   #Avatar
   @bot.command(aliases=["av"])
   async def Avatar(self, ctx,*, member : discord.Member=None):
-      useravatar = member.avatar_url
-      embed = discord.Embed(Colour = discord.Colour.orange(), title = f"Avatar for {member}", url = useravatar)
-      embed.set_image(url = f"{useravatar}")
-      await ctx.send(embed=embed)
+      if member = None:
+          member = ctx.message.author
+      else:
+        member = member
+    useravatar = member.avatar_url
+    embed = discord.Embed(Colour = discord.Colour.orange(), title = f"Avatar for {member}", url = useravatar)
+    embed.set_image(url = f"{useravatar}")
+    await ctx.send(embed=embed)
 
 
   #8Ball
