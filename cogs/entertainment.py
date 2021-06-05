8dc2f562bc05ff50768a98e597dca5232327dcfe
doughnut9 <>
2021-06-05 17:37:26 +0000

    Debugged help command.

--- cogs/entertainment.py
+++ cogs/entertainment.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 import random
 
 
-
 bot = commands.Bot(command_prefix="$")
 
 
--- cogs/help.py
+++ cogs/help.py
@@ -1,12 +1,12 @@
 import discord
-from discord.ext import commands
-from discord.ext import tasks
+import os
+from discord.ext import commands, tasks
 
 
 
 bot = commands.Bot(command_prefix="$")
 
-#"Kick - Kicks a member\nkick {member} [reason]\n\nBan - Bans a member\nban {member} [reason]\n\nUnban - Unbans a member\nunban {member} [reason]\n\nPurge - Deletes the given amount of messages\npurge {amount}\n\n Mute - Mutes the given member\nmute {member} {reason}"
+
 
 class Help(commands.Cog):
   def __init__(self, bot):
@@ -14,9 +14,10 @@ class Help(commands.Cog):
 
 
 
-    @bot.command(pass_context=True)
-    async def help(self, ctx, *, category=None):
-        if vategory == None:
+    #Help
+    @bot.command()
+    async def help(ctx, *, category=None):
+        if category == None:
           embed = discord.Embed(
             colour=discord.Colour.orange(),
             title="Help Commands for Study Café Bot",
@@ -25,37 +26,32 @@ class Help(commands.Cog):
 
           embed.set_author(name="doughnut#3968", url="https://discord.gg/8tcHNurFjp", icon_url="https://sites.google.com/site/12untitled7/_/rsrc/1298297964945/home/sasori/Sasori.jpg")
           embed.set_thumbnail(url="https://i.gifer.com/VR0F.gif")
-          embed.add_field(name="Moderation", value = "Moderation Commands, `$help moderation`", inline = True)
-          embed.add_field(name="Entertainment", value = "Entertainment Commands, `$help entertainment`", inline = True)
+          embed.add_field(name="Moderation", value = "Moderation Commands, `$help moderation`", inline = False)
+          embed.add_field(name="Entertainment", value = "Entertainment Commands, `$help entertainment`", inline = False)
           embed.add_field(name="Ping", value = "Displays the latency of the bot - `$ping`", inline = False)
           embed.add_field(name="Misc", value = "More features coming soon.", inline = True)
           embed.set_footer(text = '----------------------------------------------------------------------------------------------\nThe bot is currently under development and more commands will be added soon!\n[] - Reqired Arguments   &   {} - Optional Arguments\n')
 
-      if category == "moderation":
+        if category == "moderation":
           embed = discord.Embed(
-            colour=discord.Colour.orange(),
-            title="Moderation Commands for Study Café Bot",
-            url = "https://docs.google.com/document/d/1p1FBEqOhmvIu4otkF2YuqD_mdAnekWctFBq07UjKxbc/edit?usp=sharing"
-          )
-          embed.add_field(name="Moderation", value = "Kick - Kicks a member\n`$kick {member} [reason]`\n\nBan - Bans a member\n`$ban {member} [reason]`\n\nUnban - Unbans a member\n`$unban {member} [reason]`\n\nPurge - Deletes the given amount of messages\n`$purge {amount}`\n\n Mute - Mutes the given member\n`mute {member} {reason}`", inline = False)
+          colour=discord.Colour.orange(),
+          title="Moderation Commands for Study Café Bot",
+          url = "https://docs.google.com/document/d/1p1FBEqOhmvIu4otkF2YuqD_mdAnekWctFBq07UjKxbc/edit?usp=sharing"
+           )
+          embed.add_field(name="Moderation", value = "Kick - Kicks a member\n`$kick [member] {reason}`\n\nBan - Bans a member\n`$ban [member] {reason}`\n\nUnban - Unbans a member\n`$unban [member]`\n\nPurge - Deletes the given amount of messages\n`$purge {amount}`\n\n Mute - Mutes the given member\n`$mute [member] {reason}`", inline = False)
           embed.set_footer(text = '----------------------------------------------------------------------------------------------\nThe bot is currently under development and more commands will be added soon!\n[] - Reqired Arguments   &   {} - Optional Arguments\n')
 
-      if category == "entertainment":
+        elif category == "entertainment":
           embed = discord.Embed(
             colour=discord.Colour.orange(),
             title="Entertainment Commands for Study Café Bot",
             url = "https://docs.google.com/document/d/1p1FBEqOhmvIu4otkF2YuqD_mdAnekWctFBq07UjKxbc/edit?usp=sharing"
-          )
-          embed.add_field(name="Entertainment", value = "8Ball - Tells whether the question you asked is true or false.\n`8b {question}`\n\nAvatar - Displays the avatar of a user\n`avatar {member}`", inline = False)
+           )
+          embed.add_field(name="Entertainment", value = "8Ball - Tells whether the question you asked is true or false.\n`8b [question]`\n\nAvatar - Displays the avatar of a user\n`avatar {member}`", inline = False)
           embed.set_footer(text = '----------------------------------------------------------------------------------------------\nThe bot is currently under development and more commands will be added soon!\n[] - Reqired Arguments   &   {} - Optional Arguments\n')
+      
+        await ctx.send(embed=embed)
           
 
-
-
-
-      await ctx.send(embed=embed)
-
-
-
-      def setup(bot):
-        bot.add_cog(Help(bot))
+def setup(bot):
+  bot.add_cog(Help(bot))
